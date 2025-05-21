6894fa2b82e915ca6d9f6589a2cc76260fc24d08
Jostte12 <jostte12@gmail.com>
2025-05-21 11:48:32 +0300

    First Commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: '1',
+    question: 'What are accordion components?',
+    answer:
+      'Accordion components are user interface elements used for displaying collapsible content. They allow users to expand or collapse sections of content',
+  },
+  {
+    id: '2',
+    question: 'What are they used for?',
+    answer:
+      'They are commonly employed in various contexts, including FAQs, menus, and any situation where you want to display a lot of information without overwhelming the user.',
+  },
+  {
+    id: '3',
+    question: 'Accordian as a musical instrument',
+    answer:
+      'The accordion is a musical instrument that consists of a bellows, a keyboard, and reeds. It is played by compressing or expanding the bellows while pressing keys or buttons to produce sound.',
+  },
+  {
+    id: '4',
+    question: 'Can I create an accordion component in React?',
+    answer:
+      'Yes, you can create an accordion component in React using state management to control the visibility of the content sections. You can use hooks like useState to manage the open/closed state of each section.',
+  },
+];
+
+export default data;
